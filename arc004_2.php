665200aebff5951bee8436a3ff28462c0cc96663
wada811 <89.at.usi@gmail.com>
2012-06-17 20:37:33 +0900

    rename variable

--- arc004_2.php
+++ arc004_2.php
@@ -1,17 +1,17 @@
 <?php
     fscanf(STDIN, "%d", $n);
-    $data = array();
+    $distance = array();
     $max_distance = 0;
     for($i = 0; $i < $n; $i++){
-        fscanf(STDIN, "%d", $data[$i]);
-        $max_distance += $data[$i];
+        fscanf(STDIN, "%d", $distance[$i]);
+        $max_distance += $distance[$i];
     }
     
-    rsort($data, SORT_NUMERIC);
+    rsort($distance, SORT_NUMERIC);
     
-    $min_distance = $data[0];
+    $min_distance = $distance[0];
     for($i = 1; $i < $n; $i++){
-        $min_distance -= $data[$i];
+        $min_distance -= $distance[$i];
     }
     if($min_distance < 0){
         $min_distance = 0;
