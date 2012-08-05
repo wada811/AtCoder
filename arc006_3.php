3c64f97a067d373927082380329f9e220df0aae4
wada811 <89.at.usi@gmail.com>
2012-08-05 23:48:02 +0900

    rename variable

--- arc006_3.php
+++ arc006_3.php
@@ -5,15 +5,15 @@ for($i = 0; $i < $n; $i++){
 }
 $Mt = array();
 for($i = 0; $i < $n; $i++){
-    $min = 0;
+    $max = 0;
     for($j = 0; $j < count($Mt); $j++){
-        $pop = $Mt[$min][count($Mt[$min])-1];
+        $pop = $Mt[$max][count($Mt[$max])-1];
         if($pop < $inputs[$i]){
-            $min = $j;
+            $max = $j;
         }
     }
-    if($Mt[$min][count($Mt[$min])-1] >= $inputs[$i]){
-            $Mt[$min][] = $inputs[$i];
+    if($Mt[$max][count($Mt[$max])-1] >= $inputs[$i]){
+            $Mt[$max][] = $inputs[$i];
     }else{
             $Mt[][] = $inputs[$i];
     }
