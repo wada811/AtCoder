bf28d9a54e85a090029eb7e6fa51f1e27a2da437
wada811 <89.at.usi@gmail.com>
2012-09-09 10:47:38 +0900

    pass the AtCoder

--- tenka1_2012_B_2_preg_replace.php
+++ tenka1_2012_B_2_preg_replace.php
@@ -8,15 +8,17 @@ if(isCamelCase($variable)){
 echo $variable.PHP_EOL;
  
 function isCamelCase($str){
-    return preg_match('/^_*[a-z][a-z0-9]+([A-Z][a-z0-9]+)+_*$/', $str, $matches);
+    return preg_match('/^_*[a-z][a-z0-9]*([A-Z][a-z0-9]*)*_*$/', $str, $matches);
 }
 function toCamelCase($str){
-    return preg_replace('/([a-z0-9])_([a-z])/e', '$1.strtoupper("$2")', $str);
+    preg_match('/^(_*).*$/', $str, $matches);
+    $str = substr($str, strlen($matches[1]));
+    return $matches[1].preg_replace('/_([a-z])/e', 'strtoupper("$1")', $str);
 }
 function isSnakeCase($str){
-    return preg_match('/^_*[a-z][a-z0-9]+(_[a-z][a-z0-9]+)+_*$/', $str, $matches);
+    return preg_match('/^_*[a-z][a-z0-9]*(_[a-z][a-z0-9]*)*_*$/', $str, $matches);
 }
 function toSnakeCase($str){
-    return preg_replace('/([a-z0-9])([A-Z])/e', '$1."_".strtolower("$2")', $str);
+    return preg_replace('/([A-Z])/e', '"_".strtolower("$1")', $str);
 }
 ?>
--- tenka1_2012_B_2_preg_replace_callback.php
+++ tenka1_2012_B_2_preg_replace_callback.php
@@ -7,22 +7,28 @@ if(isCamelCase($variable)){
 }
 echo $variable.PHP_EOL;
 
+
 function isCamelCase($str){
-    return preg_match('/^_*[a-z][a-z0-9]+([A-Z][a-z0-9]+)+_*$/', $str, $matches);
+    return preg_match('/^_*[a-z][a-z0-9]*([A-Z][a-z0-9]*)*_*$/', $str, $matches);
 }
+
 function toCamelCase($str){
-    return preg_replace_callback(
-        '/([a-z0-9])_([a-z])/',
-        create_function('$matches', 'return $matches[1].strtoupper($matches[2]);'),
+    preg_match('/^(_*).*$/', $str, $matches);
+    $str = substr($str, strlen($matches[1]));
+    return $matches[1].preg_replace_callback(
+        '/_([a-z])/',
+        create_function('$matches', 'return strtoupper($matches[1]);'),
         $str);
 }
+
 function isSnakeCase($str){
-    return preg_match('/^_*[a-z][a-z0-9]+(_[a-z][a-z0-9]+)+_*$/', $str, $matches);
+    return preg_match('/^_*[a-z][a-z0-9]*(_[a-z][a-z0-9]*)*_*$/', $str, $matches);
 }
+
 function toSnakeCase($str){
     return preg_replace_callback(
-        '/([a-z0-9])([A-Z])/', 
-        create_function('$matches', 'return $matches[1]."_".strtolower($matches[2]);'),
+        '/([A-Z])/', 
+        create_function('$matches', 'return "_".strtolower($matches[1]);'),
         $str);
 }
 ?>
