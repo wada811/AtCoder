efe770393c1dd3f396b4aa29094024a692c4563a
wada811 <89.at.usi@gmail.com>
2012-09-17 14:53:53 +0900

    change code order

--- arc007_2.php
+++ arc007_2.php
@@ -3,10 +3,10 @@ fscanf(STDIN, "%d %d", $n, $m);
 for($i = 0; $i < $m; $i++){
     fscanf(STDIN, "%d", $playlist[]);
 }
-for($i = 0; $i < $n; $i++){
-    $case[] = $i + 1;
-}
 $now = 0;
+for($i = 1; $i <= $n; $i++){
+    $case[] = $i;
+}
 for($i = 0; $i < $m; $i++){
     if($now !== $playlist[$i]){
         $next = array_search($playlist[$i], $case);
