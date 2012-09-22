3fd74fda158c53cf1d5f971dd9fe1b0effad0a23
wada811 <89.at.usi@gmail.com>
2012-09-22 23:11:08 +0900

    add comment

--- arc008_2.php
+++ arc008_2.php
@@ -3,11 +3,11 @@ fscanf(STDIN, "%d %d", $n, $m);
 for($i = 0; $i < $n;$i++){
     $name[] = fgetc(STDIN);
 }
-fgetc(STDIN);
+fgetc(STDIN); // throw line feed code
 for($i = 0; $i < $m;$i++){
     $kit[] = fgetc(STDIN);
 }
-fgetc(STDIN);
+fgetc(STDIN); // throw line feed code
 
 $name_counts = array_count_values($name);
 $kit_counts = array_count_values($kit);
