f0845f57d3b9c0a2809eb394ef9ff569fa73cb27
wada811 <89.at.usi@gmail.com>
2012-10-27 19:59:09 +0900

    add dump

--- arc009_2.php
+++ arc009_2.php
@@ -11,32 +11,35 @@ fscanf(STDIN, '%d', $n);
 for($i = 0; $i < $n; $i++){
     fscanf(STDIN, '%d', $targetNumbers[]);
 }
+// echos($targetNumbers, __LINE__);
+// 14::k: 0 1 2 3 4 5 6 7 8 9
+// 14::v: 1 2 3 4 5 6 7 8 9 10
 // targetNumbers の数字をアルファベットに置換
 $alphabets = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j');
 $numbers = array_keys($alphabets);
 $targetAlphabets = str_replace($numbers, $alphabets, $targetNumbers);
 // echos($targetAlphabets, __LINE__);
-// 18::k: 0 1 2 3 4 5 6 7 8 9
-// 18::v: b c d e f g h i j ba
+// 21::k: 0 1 2 3 4 5 6 7 8 9
+// 21::v: b c d e f g h i j ba
 // このアルファベットを cardinalNumbers の順になるようにソート
 $cardinalAlphabets = array();
 for($i = 0, $end = count($cardinalNumbers); $i < $end; $i++){
     $cardinalAlphabets[$i] = $alphabets[$cardinalNumbers[$i]];
 }
 // echos($cardinalAlphabets, __LINE__);
-// 26::k: 0 1 2 3 4 5 6 7 8 9
-// 26::v: a i b d f e j h g c
-// convertNumbersIntoAlphabets を cardinalAlphabets の k に置換
+// 29::k: 0 1 2 3 4 5 6 7 8 9
+// 29::v: a i b d f e j h g c
+// targetAlphabets を cardinalAlphabets の k に置換
 $numbers = array_keys($cardinalAlphabets);
 $atCoderNumbers = str_replace($cardinalAlphabets, $numbers, $targetAlphabets);
 // echos($atCoderNumbers, __LINE__);
-// 32::k: 0 1 2 3 4 5 6 7 8 9
-// 32::v: 2 9 3 5 4 8 7 1 6 20
+// 35::k: 0 1 2 3 4 5 6 7 8 9
+// 35::v: 2 9 3 5 4 8 7 1 6 20
 // この数値を昇順に並べ替える
 asort($atCoderNumbers);
 // echos($atCoderNumbers, __LINE__);
-// 37::k: 7 0 2 4 3 8 6 5 1 9
-// 37::v: 1 2 3 4 5 6 7 8 9 20
+// 40::k: 7 0 2 4 3 8 6 5 1 9
+// 40::v: 1 2 3 4 5 6 7 8 9 20
 // この k で targetNumbers をソート
 $results = array();
 $orders = array_keys($atCoderNumbers);
@@ -44,8 +47,8 @@ foreach($orders as $order){
     $results[] = $targetNumbers[$order];
 }
 // echos($results, __LINE__);
-// 46::k: 0 1 2 3 4 5 6 7 8 9
-// 46::v: 8 1 3 5 4 9 7 6 2 10
+// 49::k: 0 1 2 3 4 5 6 7 8 9
+// 49::v: 8 1 3 5 4 9 7 6 2 10
 // 出力！
 echo implode(PHP_EOL, $results) . PHP_EOL;
 ?>
